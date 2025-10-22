ca12a7af2d486667a17bc271ec4f08a1f4087afa
CyberDagger <cyberdagger003@gmail.com>
2025-10-22 18:00:05 +0100

    Open menu

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import "./normalize.css";
 import "./styles.css";
+
+const htmlBody = document.querySelector("body");
+const menuButton = document.querySelector("#dropdown-button");
+const menuList = document.querySelector("#dropdown-list");
+
+menuButton.addEventListener("click", () => {
+    menuList.style.visibility = "visible";
+});
+
+document.addEventListener("click", (e) => {
+    if ((e.target.id != "dropdown-button") || (e.target.parentElement.id != "dropdown-button")) {
+        menuList.style.visibility = "hidden";
+    }
+})
